268c4ee79e7487cfe3d46e1b56af288b5d781b90
Amanda Truong <amt140130@utdallas.edu>
2015-11-14 00:55:50 -0600

    added code to main.asm

--- Lexathon/main.asm
+++ Lexathon/main.asm
@@ -1,9 +1,72 @@
-# LEXATHON in MIPS
+# LEXATHON in MIUPS
+# Amanda Truong, Mai Tran, Patrick Deng, Sterling Meyers
 
-.data 
+.data
+	welcome:	.asciiz "Welcome to Lexathon!\n"
+	decide:		.asciiz "Enter any key to start game or '0' for how to play \n"
+	
+	ending:		.asciiz "Enter any key to start a new game or '0' to exit \n"
+	ins1:		.asciiz "INSTRUCTIONS: \n"
+	ins2:		.asciiz "You must find as many words of four or more letters as possible in the allocated time \n"
+	ins3:		.asciiz "Each word must contain the central letter and no letter can be used twice \n"
+	ins4: 		.asciiz "You start each game with 60 seconds and you get 20 more seconds for each word found \n"
+	ins5:		.asciiz "Game ends when you either run out of time or hit 0 to give up\n"
 
 .text
 .globl main
 
-  main:
 
+
+	main:
+		#display welcome message
+		li $v0, 4
+		la $a0, welcome
+		syscall
+		
+	request:
+		#ask user to enter 0 or 1
+		li $v0, 4
+		la $a0, decide
+		syscall
+		
+		#read user's input
+		li $v0, 5
+		syscall
+		
+		#go to instructions if user entered 0
+		beq $v0, $zero, instructions 
+	GameOver:	
+		li $v0, 4
+		la $a0, ending
+		syscall
+		
+		li $v0, 5
+		syscall
+		
+		#bne $v0, $zero, startGame
+		
+		j Exit
+				
+	Exit:	
+		li $v0, 10
+		syscall
+		
+	instructions:
+		#print instructions
+		li $v0, 4
+		la $a0, ins1
+		syscall
+		
+		la $a0, ins2
+		syscall
+		
+		la $a0, ins3
+		syscall
+		
+		la $a0, ins4
+		syscall
+		
+		la $a0, ins5
+		syscall
+		
+		j request
--- Lexathon/validation.asm
+++ Lexathon/validation.asm
@@ -1,1 +1,91 @@
-init
+promptUser:		.asciiz"\n Enter your word or type '0' to give up: "
+success1:		.asciiz"\n You have found "
+success2:		.asciiz" words out of "
+success3:		.asciiz" words"	
+input:			.asciiz"         "
+test:
+	#prompt user to enter a word
+	li $v0, 4
+	la $a0, promptUser
+	syscall
+	
+	#collect user input and store in input
+	li $v0, 8
+	la $a0, input
+	li $a1, 10
+	syscall
+	
+	li $t0, 0
+	lb $t1, input($t0)
+	
+	#go to printScore if user entered 0
+	beq $t1, $zero, printScore
+	
+	#check for middle character
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal checkMiddle
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	#check if the entered word contains at least 4 characters
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal checkLength
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	#check if the entered word is a valid English word
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal checkValid
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	#check if the word has already been used
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal checkDuplicate
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	#store the correct word
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	jal storeCorrect
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	
+	#print success message
+	li $v0, 4
+	la $a0, success1
+	syscall
+	
+	li $v0, 1
+	move $a0, $s7 #$s7 hold the number of correct words
+	syscall
+	
+	li $v0, 4
+	la $a0, success2
+	syscall
+	
+	######
+	
+	li $v0, 4
+	la $a0, success3
+	syscall
+	
+	j test
+	
+	
+checkMiddle:
+
+checkLength:
+
+checkValid:
+	
+checkDuplicate:
+
+storeCorrect:
+	
+	
